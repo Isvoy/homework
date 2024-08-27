d7c01503186e9b1ce0ca1f9a5b3ced44a531a050
Isvoy <168728576+Isvoy@users.noreply.github.com>
2024-08-27 11:55:18 +0400

    Add files via upload

---
+++ финальная вёрстка перед дипломной/js/main.js
@@ -0,0 +1,70 @@
+(function(){
+
+//бургер
+
+  document. addEventListener('click', burgerInit)
+
+  function burgerInit(e){
+
+    const burgerIcon = e.target.closest('.burger-icon')
+    const burgerNavLink = e.target.closest('.nav__link')
+
+  if (!burgerIcon && !burgerNavLink) return
+  if (document.documentElement.clientWidth >900) return
+  
+  if(!document.body.classList.contains('body--opened-menu')) {
+  document.body.classList.add('body--opened-menu')
+  } else {
+    document. body. classList.remove('body--opened-menu')
+  }
+
+  }
+
+//модалка
+
+const modal = document.querySelector('.modal')
+const modalButton = document.querySelector('.about__img-button')
+
+modalButton.addEventListener('click', openModal)
+modal.addEventListener('click', closeModal)
+
+function openModal(e) {
+  e.preventDefault()
+  document.body.classList.toggle('body--opened-modal')
+}
+
+function closeModal(e) {
+  e.preventDefault()
+
+  const target = e.target
+
+  if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+    document.body.classList.remove('body--opened-modal')
+  }
+
+}
+
+//табы
+
+const tabControls = document.querySelector('.tab-controls')
+
+tabControls.addEventListener('click', toggleTab)
+
+function toggleTab(e) {
+
+  const tabControl = e.target.closest('.tab-controls__link')
+
+  if (!tabControl) return
+
+  e.preventDefault()
+
+  const tabContentID = tabControl.getAttribute('href')
+
+  document.querySelector('.tab-content--show').classList.remove('tab-content--show')
+  document.querySelector(tabContentID).classList.add('tab-content--show')
+
+  document.querySelector('.tab-controls__link--active').classList.remove('tab-controls__link--active')
+  tabControl.classList.add('tab-controls__link--active')
+}
+
+})()
